7a34ae0b32666d4a4bef24c530c02c2b185a38b6
Palathip <windeesel365@gmail.com>
2023-10-05 21:57:16 +0700

    feat(ex1 ex2):created and done

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,18 @@
 // Exercise #1: For Each Function
 
+function addSalaries(number){
+  return number + 5000;
+}
+
 function forEach(array, operation) {
   // Start coding here
+  return array.map(number => operation(number));
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+let newEmployeeSalaries = [];
 
 // Using `forEach` function here
 
+newEmployeeSalaries = forEach(employeeSalaries, addSalaries);
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,12 @@
 //Exercise #2: At Least Five Function
+function isScoreOver70Pts(score) {
+  return score>70;
+}
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let filterArray = array.filter(operation); //let filterArray = array.filter(score => score > 70);
+  return filterArray.length >= 5;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +15,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, isScoreOver70Pts);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, isScoreOver70Pts);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, isScoreOver70Pts);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
